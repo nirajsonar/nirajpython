b256a6e6e178d12e404f87880c05f9c85ce0ac0a
nirajsonar <117706379+nirajsonar@users.noreply.github.com>
2022-11-09 21:36:25 +0530

    Add files via upload

---
+++ niraj.py
@@ -0,0 +1,124 @@
+# #### OPERATORS  #####
+# 1 arithmatic operator
+# 2 assignment operator
+# 3 comparision operator
+# 4 logical operator
+# 5 identity operator
+# 6 membership operator
+
+#1) ARITHMATIC OPERATOR ( + , - ,* , / , // , ** , % ,)
+
+#a=3
+#b=5
+#addition 
+#print(a+b)
+
+# sub
+# print(a-b)
+
+# multi
+# print(a*b)
+
+# divi
+# print(a/b)
+
+# module
+# print(a%b)
+
+# floor divison 
+# print(a//b) 
+
+# 2 ) ASSIGNMENT OPERATOR
+
+#(equal)
+a=5 
+
+#equal
+#a==5
+#print(a)
+
+# (plus)
+# #a+=2      
+# #print(a)
+
+# (sub)
+# a-=5
+# print(a)
+
+#multi
+#a*=5
+#print(a)
+
+# (divide)
+# a/=2
+# print(a)
+
+# (floor div)
+# a//=5
+# print(a)
+
+#(exponentiion square root)
+#a**=5
+#print(a)
+
+#(and)
+#a&=5
+#print(a)
+
+# 3 COMPARISION OPERATOR
+
+a=10
+b=20
+
+#(equal)
+#print(a==12)
+
+#(not equal)
+#print(a!=11)
+
+#(greater than)
+#print(a<15)
+
+#(less than)
+#print(a>12)
+
+#(greater than oe equal)
+#print(a>=15)
+
+#(less than or equal)
+#print(a<=10)
+
+# 4 ) LOGICAL OPERATOR( and , or, not)
+
+# a=10
+# b=5
+
+#( and )
+# print(a>5 and a<20)
+
+#( or )
+# print(a<5 or a<9)
+
+#( not )
+# print(not(a<5 and a<5))
+
+# 5 ) IDENTITY OPERATOR(is, is not)
+
+# a=10
+# b=20
+
+#( is )
+# print(a is b)
+
+#( is not )
+# print(a is not b)
+
+# 6 MEMBERSHIP OPERATOR (in, not in)
+
+# a=1,2,3,4,5
+
+#( in )
+# print(6 in a)
+
+#(not in)
+# print(1 not in a)
