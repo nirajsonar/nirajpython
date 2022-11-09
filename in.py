bdd76e53ed21c32e20e105f613a9d3c71c84a97a
nirajsonar <117706379+nirajsonar@users.noreply.github.com>
2022-11-09 21:48:03 +0530

    Add files via upload

---
+++ in.py
@@ -0,0 +1,7 @@
+# program for addition
+num1 = input ('enter first number:')
+num2 = input ('enter second number:')
+
+#add two numbers
+sum = float(num1) + float(num2)
+print('the sum of {1} and {2} is {3}'.format(num1,num2,sum))
