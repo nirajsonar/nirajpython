d15cb1b71091c1411bee4f8a4643b17e3a956f26
nirajsonar <117706379+nirajsonar@users.noreply.github.com>
2022-11-15 19:32:43 -0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,65 @@
+# 1) we can be able to use any type of data
+
+# syntax :- stores square braket [ ] 
+#           it is a muteble formation
+
+#lst=[1,2,3,4,5,"niraj",1.2,5.1,True]
+#print(lst)
+#print(type(lst))
+
+#methods :-
+
+lst=[1,5,1,3,4,8,6,7]
+
+# 1) sort :- (sorting of no)
+#lst.sort()
+#print(lst)
+
+# 2) reverse :- ( element reverse)
+#lst.reverse()
+#print(lst)
+ 
+# 3) append  :-( carry only single element)
+#lst.append(5)
+#print(lst)
+
+# 4) extend :-( multiple element)
+#lst.extend([10,2,3,4])
+#print(lst)
+
+# 5) pop :- (remove last element)
+#lst.pop()
+#print(lst)
+
+# 6) remove :-( remove no as require)
+#lst.remove(3)
+#print(lst)
+
+# 7) len :- (lenth of lst)
+#print(len(lst))
+
+# 8) sum:- (sum of elements)
+#print(sum(lst))
+
+# 9) index :-
+#lst[2]=5
+#print(lst)
+
+# 10) clear :-
+#lst.clear() 
+#print(lst)
+
+# 11) delete :- (element delete)
+#del lst[1]
+#print(lst)
+
+# 12) count :- (sum of all element)
+#print(lst.count(1))
+
+# 13) insert :- (insert at index no)
+#lst.insert(2,5)
+#print(lst)
+
+# 14) copy :- (copy element original list to new list)
+#new=lst.copy()
+#print(new)
---
+++ operator.py
@@ -0,0 +1,124 @@
+########################## 1) arithmatical operator :-
+
+a=10
+b=20
+
+#addition
+#print(a+b)
+
+#substraction
+#print(a-b)
+
+#multiplication
+#print(a*b)
+
+#divide
+#print(a/b)
+
+#module
+#print(a%b)
+
+#exponentiation
+#print(a**b)
+
+#floor division
+#print(a//b)
+
+####################### 2)Assignment operator:-
+
+a=5
+
+#addition
+#a+=5
+#print(a)
+
+#substraction
+#a-=5
+#print(a)
+
+#multi
+#a*=5
+#print(a)
+
+#divide
+#a/=5
+#print(a)
+
+#module
+#a%=5
+#print(a)
+
+#floor division
+#a//=5
+#print(a)
+
+#exspentiation
+#a**=5
+#print(a)
+
+#and
+#a&=5
+#print(a)
+
+######################## 3)comparision operator:-
+
+a=5
+
+#equal
+#print(a==5)
+
+#not equal
+#a!=5
+#print(a!=5)
+
+#greater than
+#a>10
+#print(a>10)
+
+#less than
+#a<10
+#print(a<20)
+
+#greater than or equal
+#a>=5
+#print(a>=6)
+
+#less than or equal
+#a<=5
+#print(a<=5)
+
+
+############################  4)logical operator :-
+
+a=10
+
+#and
+#print(a<20 and a>5)
+
+#or
+#print(a<30 or a>10)
+
+#not
+#print(not(a<10 and a<30))
+
+############################ 5) Identity operator:-
+
+a=10
+
+#is
+#print(a is 10)
+
+#is not
+#print(a is not 20)
+
+########################## 6) membership operator:-
+
+a=[1,2,3,4,5,6]
+
+#in
+#print( 5 in a)
+
+#not in
+#print(20 not in a)
+
+
