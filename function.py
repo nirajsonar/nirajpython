821cb5c0c985dac00b1353e7e55af01bb4e846ab
nirajsonar <117706379+nirajsonar@users.noreply.github.com>
2022-11-15 20:39:46 -0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,27 @@
+# def add():
+#     a=20
+#     b=10
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+#     print(a/b)
+# add()
+
+#def sum(a,b,c):
+#    print(a+b+c)
+#sum(10,20,30)
+
+#def greeting(a):
+#    print(a)
+#greeting('good morning')
+
+#def add(a,c,d,b):
+#     print(a+b+c+d)
+#add(a=10,b=20,c=30,d=40)     
+
+def family(c3,c2,c1):
+     print(c2)
+family(c3='sagar',c2='niraj',c1='nikhil')
+
+
+
