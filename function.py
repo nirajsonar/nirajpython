9e4abf6b542a5010110f7c34cc7d68ac76450176
nirajsonar <117706379+nirajsonar@users.noreply.github.com>
2022-11-16 21:06:30 -0800

    Update function.py

--- function.py
+++ function.py
@@ -27,5 +27,74 @@ def fam(c3='sagar',c2='niraj',c1='nikhil'):
     print(c2)
 fam()
 
+#def institute(institude='tecknocraft'):
+#    print('institude name is,institude')
+#inst('njsdnv')
+#inst('bhguy')
+#inst(' ')
+
+#def name(name='niraj sonar'):
+#    print('my name is ,name')
+#    first='niraj'
+#    last='sonar'
+#    fullname=first+' '+last
+#name()
+
+#def avg():
+#    a=10
+#    b=20
+ #   c=30
+#    avg=(a+b+c)/3
+#    print(avg)
+#avg()
+
+#def avg():
+#    a=int(input('enter a'))
+#    #print(type(a))
+#    b=int(input('enter b'))
+#    c=int(input('enter c'))
+#    avg=(a+b+c)/3
+#    print(avg)
+#avg()
+
+###################################### pass
+
+#def add():
+ #   pass
+#add()
+
+#################################  lambda
+#1 decrease line of Code
+#2 easy for execution
+#3 easy syntax
+
+#def add(a,b):
+#    print(a+b)
+#add(20,30,)
+
+#add=lambda a,b:(a+b)
+#print(add(20,30))
+
+#cube=lambda a:(a*a*a)
+#print(cube(3))
+
+#def lst(fruits):
+#    print(fruits)
+#fruits=['apple','orange']
+#lst(fruits)
+
+#def tup(fruite):
+#    print(fruite)
+#fruite=('apple','orange')
+#tup(fruite)
+
+def lst(fruite):
+    fruite.append('banana')
+    print(fruite)
+fruite=['apple','orange']
+lst(fruite)
+
+
+
 
 
