8001f74f951c1314a002dddf16d5368647f0e689
nirajsonar <117706379+nirajsonar@users.noreply.github.com>
2022-11-17 21:18:01 -0800

    Add files via upload

---
+++ if.py
@@ -0,0 +1,80 @@
+#if else
+#
+
+#x= 2
+
+#if x==1:
+#    print("yes")
+#if x==3:
+#    print("yes")
+#if x==2:
+#    print("yes")
+
+
+#a=int(input("enter any number"))
+#if (a>50):
+#    print("greater")
+#if (a<20):
+#  print("smaller")
+#else:
+#    print("smaller")
+
+#age=int(input("enter age : "))
+# if (age<10):
+#    print("child")
+#elif (age<20):
+#    print("teen")
+#elif(age>20):
+#   print("adult")
+#elif (age>100):
+#    print("wrong age")
+#else:
+#    print("old")
+
+#num=int(input("enter any number  : "))
+#if(num % 2==0):
+#    print("even")
+#else:
+#    print("odd")
+
+
+# nested if :-
+
+#a=int(input("enter any no"))
+#if (a<30):
+#    if a%2==0
+#    print("smaller")
+#else:
+#    print("no")
+
+#name=input("enter society name")
+#if name=="sai":
+#    print("please call on 7397656441")
+#else:
+#    print("go")
+
+
+#name=input("enter society name")
+#flatno=int(input("enter no"))
+#if name=="sai"or"aaa":
+#    if flatno==10:
+#        print("call")
+#    elif flatno==20:
+#        print("call 55645456")
+#else:
+#    print("wrong")
+
+society=input("enter society name ")
+email=int(input("enter email"))
+name=input("sis name")
+if society=="niraj":
+    print("enter email")
+elif email=='12345':
+    print("enter sis name")
+if name=="lalita":
+    print("call")
+else:
+    print("ghari  ja")
+
+
+    
